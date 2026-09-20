4890f3d4447ebc17493b9df802a340a754983aef
t11536226 <t11536226@ntut.edu.tw>
2026-09-20 15:43:19 +0800

    till 26 ok

--- HW2.25/source/main.c
+++ HW2.25/source/main.c
@@ -2,15 +2,23 @@
 #include<stdlib.h>
 int main()
 {
-	int a,b,c,d,M,m = 0;
-	printf("input 3 numbers:");
-	scanf_s("%d %d %d", &a, &b, &c);
-	//printf("%d\n",a);
-	//printf("%d\n",b);
-	//printf("%d\n",c);
-	M = a; m = a;
-	if (b > M) M=b;if(c > M) M=c;
-	if (b < m) m=b;if(c < m) m=c;
-	printf("largest = %d,smallest = %d",M,m);
-	return 0;
+    printf("PPPPPPPPP\n");
+    printf("    P   P\n");
+    printf("    P   P\n");
+    printf("    P   P\n");
+    printf("     P P \n\n");
+
+    printf("  JJ     \n");
+    printf(" J       \n");
+    printf("J        \n");
+    printf(" J       \n");
+    printf("  JJJJJJJ\n\n");
+ 
+    printf("DDDDDDDDD\n");
+    printf("D       D\n");
+    printf("D       D\n");
+    printf(" D     D \n");
+    printf("  DDDDD  \n");
+
+    return 0;
 }
---
+++ HW2.26/source/main.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	int a,b,c,d,M,m = 0;
+	printf("input 2 numbers:");
+	scanf_s("%d %d", &a, &b);
+	//printf("%d\n",a);
+	//printf("%d\n",b);
+	//printf("%d\n",c);
+	if (a % b == 0) c = 1; 
+	else  c = 0;
+	if(c==1) printf("%d is %d's multpie", a, b);
+	else    printf("%d is not %d's multpie", a, b);
+	return 0;
+}
