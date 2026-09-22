0cca15f3b42b88100d5a592c5824021d8171395b
115360226 <t115360226.ntut.org.tw>
2026-09-22 20:30:20 +0800

    done

--- HW2.23/source/main.c
+++ HW2.23/source/main.c
@@ -1,17 +1,19 @@
 #include<stdio.h>
 #include<stdlib.h>
+#define _CRT_SECURE_NO_WARNINGS
 int main()
 {
-	int a,b,c,d,M,m = 0;
+	int a, b, c, d, M, m = 0;
 	printf("input 3 numbers:");
 	scanf_s("%d %d %d", &a, &b, &c);
+	
 	//printf("%d\n",a);
 	//printf("%d\n",b);
 	//printf("%d\n",c);
 	M = a; m = a;
-	if (b > M) M=b;if(c > M) M=c;
-	if (b < m) m=b;if(c < m) m=c;
-	printf("largest = %d,smallest = %d\n",M,m);
+	if (b > M) M = b; if (c > M) M = c;
+	if (b < m) m = b; if (c < m) m = c;
+	printf("largest = %d,smallest = %d\n", M, m);
 	system("pause");
 	return 0;
 }
